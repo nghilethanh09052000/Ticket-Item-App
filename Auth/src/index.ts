e7d8e8b14d6a12e744c5be0016414682ab0c663f
Your Name <nghilt19411@gmail.com>
2022-09-26 23:37:30 +0700

    add error handler in error and fix postman issue

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,14 +1,36 @@
 import  express  from "express";
+import 'express-async-errors'
 import {json} from 'body-parser';
 
-const app = express();
+// Routes Handler:
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
+// Middleware:
+import { errorHandler } from "./middlewares/error-handler";
+
+// Not found Error:
+import { NotFoundError } from "./errors/not-found-error";
+
+
+
+const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser',(req,res)=>{
-    res.send('Hi there!')
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+app.all('*', async (req,res,next) => {
+    throw new NotFoundError();
 })
 
+app.use(errorHandler)
+
+
 app.listen(3000,()=>{
-    console.log('Listening on port 3000')
+    console.log('Listening on port 3000!')
 })
