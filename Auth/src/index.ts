38471284fa21af718623741d76eb6fbbdf7dc714
Your Name <nghilt19411@gmail.com>
2022-09-28 00:43:50 +0700

    add mongo database and add signup process

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,6 +1,7 @@
 import  express  from "express";
 import 'express-async-errors'
 import {json} from 'body-parser';
+import mongoose from "mongoose";
 
 // Routes Handler:
 import { currentUserRouter } from "./routes/current-user";
@@ -30,7 +31,18 @@ app.all('*', async (req,res,next) => {
 
 app.use(errorHandler)
 
-
-app.listen(3000,()=>{
-    console.log('Listening on port 3000!')
-})
+const start = async () => {
+    try
+    {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to mongodb')
+    }
+    catch(error)
+    {   
+        console.log(error)
+    }
+    app.listen(3000,()=>{
+        console.log('Listening on port 3000!')
+    })
+}
+start();
--- Auth/src/routes/signup.ts
+++ Auth/src/routes/signup.ts
@@ -1,9 +1,19 @@
+/** 
+    Bad request handler
+**/
 import { body, validationResult } from "express-validator";
 import express,{Request,Response}  from "express";
+
+/*
+    Bad request handler
+*/
 import { RequestValidationError} from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
-
+/*
+    Models
+*/
+import { User } from "../models/users";
 
 const router = express.Router();
 
@@ -16,15 +26,23 @@ router.post('/api/users/signup', [
         .isLength({min:4,max:20})
         .withMessage("Password must be between 4 and 20 characters")
 ],
-(req:Request,res:Response)=>{
+async (req:Request,res:Response)=>{
 
     const errors = validationResult(req);
     if(!errors.isEmpty()) throw new RequestValidationError(errors.array());
     
     const { email, password } = req.body;
-    console.log('Creating a user');
-    throw new DatabaseConnectionError();
-    res.send({})
+    const existingUser = await User.findOne({email});
+
+    if(existingUser)
+    {
+        throw new BadRequestError('Email in use')
+    }
+    const user = User.build({email,password})
+    await user.save();
+    res.status(201).send(user)
+    
+    //res.send({})
     
 })
 
