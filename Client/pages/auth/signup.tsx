32df5677e9a48755624ef07ab49d2f10edee6d86
Your Name <nghilt19411@gmail.com>
2022-09-30 17:17:02 +0700

    push image files

---
+++ Client/pages/auth/signup.tsx
@@ -0,0 +1,11 @@
+
+
+const Signup = () => {
+    return (
+        <form>
+            <h1>Sign Up</h1>
+        </form>
+      );
+}
+ 
+export default Signup;
---
+++ prebuildImage.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SET CUR_DIR=%~dp0
+ECHO %CUR_DIR%
+
+cd %CUR_DIR%/Auth
+call docker build -t nghilt19411/auth .
+call docker push nghilt19411/auth
+
+cd .. 
+
+cd %CUR_DIR%/Client
+call docker build -t nghilt19411/client .
+call docker push nghilt19411/client
