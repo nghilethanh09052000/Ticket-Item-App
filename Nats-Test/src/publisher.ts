f479eb56ae326668e00c55e2815b6bc3c2c574e6
Your Name <nghilt19411@gmail.com>
2022-10-09 23:29:31 +0700

    update all

--- Nats-Test/src/publisher.ts
+++ Nats-Test/src/publisher.ts
@@ -1,18 +1,18 @@
 import nats from 'node-nats-streaming'
 
-
-const client = nats.connect('ticketing','abc', {
+console.clear();
+const stan = nats.connect('ticketing','abc', {
     url:'http://localhost:4222'
 });
 
-client.on('connect',()=>{
+stan.on('connect',()=>{
     console.log('Publisher connect to nats');
     const data = JSON.stringify({ 
         id:'123',
         title:'concert',
         price:20
     })
-    client.publish('ticket:created',data,()=>{
+    stan.publish('ticket:created',data,()=>{
         console.log('Event Publish')
     })
 })
