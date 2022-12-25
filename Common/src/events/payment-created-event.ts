8353cefdef2e88a01e4b794ba0c98cadd7a51b9e
thanhnghi0905200 <nghilt19411@gmail.com>
2022-12-25 17:15:37 +0700

    Updates

---
+++ Common/src/events/payment-created-event.ts
@@ -0,0 +1,13 @@
+import { Subjects } from "./subjects";
+
+
+
+
+export interface PaymentCreatedEvent {
+    subject: Subjects.PaymentCreated;
+    data: {
+        id: string;
+        orderId: string;
+        stripeId: string
+    };
+}
